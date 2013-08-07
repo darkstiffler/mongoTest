70e85c1b97c395b7c49e94efc775ffd8af616884
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-08-07 10:40:17 -0700

    initial commit, add RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongoTest::Application.config.secret_key_base = '4fed2c8299f609c59bc5a0bf20ea8bb7d9d59ca9128c0d08e90da13d5a2ed112d760af826ae1a4d55579622f491331eb4254ab290c1da6775bd181f2963d488c'
